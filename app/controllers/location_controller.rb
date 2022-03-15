b1e68f8b90c461e3307e790f85b669c5fbf82243
Scott Roberts <scott.lee.roberts@gmail.com>
2022-03-15 14:00:00 +0100

    Refactoring for more idiomatic rails/ruby

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,41 +1,42 @@
 class LocationController < ApplicationController
+  before_action :initialize_location
+
   def show
-    @location = location
     return unless @location.valid?
 
-    @weather, @cache_hit = fetch_weather(@location)
+    @weather, @cache_hit = fetch_weather
+    #
+    # reset the location since we have weather data to give user a blank form
+    @location = Location.new
   end
 
   private
+
+  def initialize_location
+    @location = Location.new(location_params)
+  end
 
   def location_params
     params.permit(:city, :state, :country, :zip)
   end
 
-  def location
-    Location.new(location_params)
-  end
-
-  def fetch_weather(location)
+  def fetch_weather
     # Look up the latitude and longitude of the location.
-    lat, long = LocationLookup.lookup(location)
+    lat, long = LocationLookup.lookup(@location)
 
     # return if there are not lat/long
     return [nil, false] unless lat && long
 
-    # use the zip as the cache key
-    cache_key = location.zip
-
-    cache_hit = Rails.cache.exist?(cache_key)
-
     # Look up the weather data for the latitude and longitude.
+    cache_key = @location.zip
+    cache_hit = Rails.cache.exist?(cache_key)
     weather = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
       WeatherLookup.lookup(lat, long)
     end
 
-    # reset the location since we have weather data to give user a blank form
-    @location = Location.new
-
     [weather, cache_hit]
+  rescue StandardError => e
+    Rails.logger.error("Error fetching weather: #{e.message}")
+    [nil, false]
   end
 end
