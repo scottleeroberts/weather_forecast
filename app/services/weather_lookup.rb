91e00322bde0976a3582e510a823768ec1ade3ca
Scott Roberts <scott.lee.roberts@gmail.com>
2022-03-15 14:00:00 +0100

    Refactoring to make code more idiomatic

--- app/services/weather_lookup.rb
+++ app/services/weather_lookup.rb
@@ -1,28 +1,29 @@
 class WeatherLookup
+  BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'.freeze
+
   def self.lookup(lat, lon)
     return Weather.new if lat.nil? || lon.nil?
 
-    url = 'http://api.openweathermap.org/data/2.5/weather'.freeze
-    headers = { 'Content-Type' => 'application/json' }
     params = {
       appid: '9f4af7832c15d9048ae1a43dcae3b5a8',
       lat:,
       lon:,
       units: 'metric'
     }
-    response = Faraday.get(url, params, headers)
+    response = Faraday.get(BASE_URL, params, headers)
 
     raise StandardError, "Error Getting Weather Data: #{response.status}" unless response.status == 200
 
     body = JSON.parse(response.body)
-    temp = body['main']['temp']
-    low_temp = body['main']['temp_min']
-    high_temp = body['main']['temp_max']
 
     Weather.new(
-      'temperature' => temp,
-      'low_temperature' => low_temp,
-      'high_temperature' => high_temp
+      temperature: body.dig('main', 'temp'),
+      low_temperature: body.dig('main', 'temp_min'),
+      high_temperature: body.dig('main', 'temp_max')
     )
+  end
+
+  def self.headers
+    { 'Content-Type' => 'application/json' }
   end
 end
